54bb4f2bc7b86c14874ac7d5374f770995d9e917
Zotman03 <zhenglin.ning@outlook.com>
2023-05-14 18:40:13 -0700

    navbar

--- src/NavBar.js
+++ src/NavBar.js
@@ -1,18 +1,35 @@
-import "./NavBar.css";
+import { BrandGithub, BrandLinkedin, CurrencyRipple } from "tabler-icons-react";
+
+const handleDownload = () => {
+  const url = "./Ning_Resume.pdf";
+  const link = document.createElement("a");
+  link.href = url;
+  link.download = "Ning_Resume.pdf";
+  link.click();
+};
 
 export default function Navbar() {
   return (
-    <div className="navbar bg-base-100 flex-1 bg-[#8c1515]">
-      <div className="flex-1 text-white">David Ning's Website</div>
-      <div className="flex-none list-none">
-        <ul className="menu menu-horizontal px-1">
-          <li>
-            <a href="https://ningzhenglin.github.io">Home</a>
-          </li>
-          <li>Research</li>
-          <li>Experience</li>
-        </ul>
+    <>
+      <div className=" navbar bg-base-100 bg-[#167e66] grid justify-items-center grid-cols-8 text-white text-lg menu menu-horizontal px-1">
+        <CurrencyRipple size={48} strokeWidth={2} color={"black"} />
+        <button className="text-white">
+          <a href="https://ningzhenglin.github.io">David's Personal Website</a>
+        </button>
+
+        <button>
+          <a href="https://ningzhenglin.github.io">Home</a>
+        </button>
+        <button className="w-24">Research</button>
+        <button className="w-24">Experience</button>
+        <button onClick={handleDownload}>CV Download</button>
+        <button>
+          <BrandGithub size={35} strokeWidth={2} color={"white"} />
+        </button>
+        <button>
+          <BrandLinkedin size={35} strokeWidth={2} color={"white"} />
+        </button>
       </div>
-    </div>
+    </>
   );
 }
